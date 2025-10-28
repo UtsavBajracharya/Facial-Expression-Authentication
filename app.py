5e0f046d96abc949c84dca74ebae5a68ff2eb3c1
Utsav <utsavbajra619@gmail.com>
2025-10-27 23:59:38 -0400

    add register page

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ def index():
         return redirect(url_for('dashboard'))
     return render_template('index.html')
 
+# Registration page
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
